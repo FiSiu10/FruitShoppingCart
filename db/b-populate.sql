1bda086d6866252bb58bcfd7ad9d62eba90bd7f5
Roy Jeong <royjeong@Roys-MacBook-Pro.local>
2017-05-09 19:21:07 -0700

    create a-schema.sql, b-populate.sql, .mwb file for db design, .png file for db design

--- db/b-populate.sql
+++ db/b-populate.sql
@@ -1,4 +1,23 @@
-INSERT INTO tblNames
-	(first_name, last_name)
+INSERT INTO product
+    (prod_name, prod_desc, stock_amount, unit_price, photo)
 VALUES
-	('Robot', 'Kyle');
+    ('Durian', 'Popular in Southeast Asia, the durian is named after its spiky exterior. The husk, distinct smell, and size of the durian make the fruit distinctive.', '100', '$7.00','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/durian.jpg'),
+    ('Rambutan', 'The rambutan is named after the hair skin covering it. The fruit inside is sweet, and contains a seed inside.', '200', '$0.35','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Rambutan.jpg'),
+    ('Mangosteen', 'The mangosteens fruity interior is tangy and sweet, while the hard purple rind is not edible.', '72', '$0.20','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/PurpleMangosteen.jpg'),
+    ('Salak', 'Snake fruit can be compared to pineapple in taste, with a tangy and sweet flavour. The name is from the scaly skin covering the fruit.', '34', '$0.70','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/SnakeFruit.jpg'),
+    ('Carambola', ' The star fruit is named for it is star-like appearance when cut in cross-section. The entire fruit can be eaten, providing juicy flavour with a crunch.', '99', '$0.60','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/StarFruit.jpg'),
+    ('Dragonfruit', 'The dragonfruits name comes from the appearance of dragon flames or scales on its exterior. The flavour is mild and bland, with the fun coming from distinctive exterior.', '176', '$5.50','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Dragonfruit.jpg'),
+    ('Longan', ' The longans name comes from how the shelled fruit looks like an eyeball (due to a single black seed inside). The taste is sweet and comparable to that of a lychee.', '300', '$0.05','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Longan.jpg'),
+    ('Lychee', 'Lychee fruits are small and very sweet. The reddish outside is not edible, but the interior is typically eaten when fresh.', '154', '$0.05','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Lychee.jpg'),
+    ('Breadfruit', 'Popular in Southeast Asia, the durian is named after its spiky exterior. The husk, distinct smell, and size of the durian make the fruit distinctive.', '21', '$2.00','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Breadfruit.jpg'),
+    ('Indian Mulberry', ' The fruit has a bitter flavour and a strong smell (hence the name), but the seeds are can be roasted and eaten.', '65', '$1.00','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/CheeseFruit.jpg'),
+    ('Pomelo', 'The pomelo is a large citruis fruit contained in a thick rind. The flavour is sweet, and both the interior and the rind are edible.', '100', '$3.50','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Pomelo.jpg'),
+    ('Sapodilla ', 'Popular in Southeast Asia, the durian is named after its spiky exterior. The husk, distinct smell, and size of the durian make the fruit distinctive.', '120', '$0.20','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Sapodilla.jpg'),
+    ('Water Apple', 'The water apple is actually a berry. It is a juicy and crunchy fruit.', '15', '$1.00','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/WaterApple.jpg'),
+    ('Guava', 'Guavas commonly have green exteriors and pink interiors when ripe. It can be served raw as is, with some spices, or with sugar.', '99', '$1.25','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Guava.jpg'),
+    ('Kumquat', 'Kumquats are similar in size to large olives, but they look like oranges. The fruit is often turned into jelly or marmalade, but can also be eaten cooked.', '500', '$0.10','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Kumquat.jpg'),
+    ('Waxberry', 'This fruit provides a sweet, but tart flavour with one seed inside. The fruit can be eaten dried or fresh, or even fermented into liquor.', '63', '$0.10','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Yangmei.jpg'),
+    ('Soursop', ' This fruits flavour can be compared to combining a pineapple and a strawberry. The texture is creamy like a banana or coconut.', '42', '$0.35','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Soursop.jpg'),
+    ('Pulasan', 'This fruit is sweet and typically eaten fresh. The seed inside can also be eaten raw.', '80', '$0.20','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/Pulasan.jpg'),
+    ('Kaffir Lime', 'The green skin of this fruit is bumpy and rough. The exterior, zest, and the leaves are all edible.', '121', '$0.20','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/KaffirLime.jpg'),
+    ('Sugar Apple', 'The flesh of the fruit is sweet, and could be compared to custard in taste. There may be multiple seeds inside that are not edible.', '29', '$1.50','http://deepblue.cs.camosun.bc.ca/~cst611/ics199images/SugarApple.jpg');
