8e56997cf9990011b5429c1da8361566021b1050
Connor Zubrecki <czubrecki@gmail.com>
2017-05-17 11:06:00 -0700

    addToCart.php

--- src/model/db_functions.php
+++ src/model/db_functions.php
@@ -54,6 +54,18 @@ function getProductInfo($prod_id) {
 		return $productInfo;
 }
 
+function getCartInfo($prod_id) {
+    global $dbc;
+
+		$query = 'SELECT prod_name, unit_price FROM product WHERE prod_id = (:prod_id)';
+		$statement = $dbc->prepare($query);
+    $statement->bindValue(':prod_id', $prod_id);
+		$statement->execute();
+		$productInfo = $statement->fetchAll();
+		$statement->closeCursor();
+		return $productInfo;
+}
+
 
 /**
  * This function takes in a first and last name
@@ -120,7 +132,7 @@ function getAllNames()
 }
 */
 
-/** 
+/**
  * This function takes in a address, city, province, and country
  * and stores it in the database
  *
@@ -128,22 +140,22 @@ function getAllNames()
  * @param string $city - the city the user entered in the form.
  * @param string $province - the province the user entered in the form.
  * @param string $postalcode - the postal code the user entered in the form.
- * @param string $country - the country the user entered in the form.  
+ * @param string $country - the country the user entered in the form.
  *
- * @return void 
+ * @return void
  */
 function storeBillingAddress($address, $city, $province, $postalcode, $country)
 {
     global $dbc;
-    
-    $query = 'INSERT INTO customer_order (bill_addr, bill_city, bill_prov, bill_pc, bill_country) 
+
+    $query = 'INSERT INTO customer_order (bill_addr, bill_city, bill_prov, bill_pc, bill_country)
         		VALUES (:address, :city, :province, :postalcode, :country)';
     $statement = $dbc->prepare($query);
     $statement->bindValue(':address', $address);
     $statement->bindValue(':city', $city);
     $statement->bindValue(':province', $province);
-    $statement->bindValue(':postalcode', $postalcode);	
-    $statement->bindValue(':country', $country);        
+    $statement->bindValue(':postalcode', $postalcode);
+    $statement->bindValue(':country', $country);
     $statement->execute();
     $statement->closeCursor();
 }
