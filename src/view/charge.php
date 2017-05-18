df481d51cf9389f15a4bdc4c3cebd705e5340fee
Kerensa Mattison <kcamosun@gmail.com>
2017-05-18 14:55:11 -0700

    Changed Stripe pages.

--- src/view/charge.php
+++ src/view/charge.php
@@ -1,5 +1,5 @@
 <?php
-require_once('./config.php');
+require_once('config.php');
 require('PHPMailer/PHPMailerAutoload.php');
 $token = filter_input(INPUT_POST, 'stripeToken', FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'stripeEmail', FILTER_VALIDATE_EMAIL);
