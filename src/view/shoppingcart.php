e7404e8fd5fb0e4253ddf488e332f3c34e8e1798
Fiona Siu <fiona.siu10@gmail.com>
2017-05-18 14:53:06 -0700

    fixed item display and totals in shoppingcart.php

--- src/view/shoppingcart.php
+++ src/view/shoppingcart.php
@@ -10,21 +10,10 @@ for ($i = 0; $i < count($_SESSION['itemQty']); $i++) {
     for ($j = 0; $j < count($productInfo); $j++) {
         $prod[$i] = $productInfo[$j];
     }
-
-    //Calculate item total
-    for ($k = 0; $k < count($_SESSION['itemQty']); $k++) {
-        $total = (int) $_SESSION['itemQty'][$k] * (int) $prod['unit_price'];
-        $subtotal = $total + $total;
-    }
-
-    $grandtotal = $subtotal + 10;
+    $total[$i] = (int) $_SESSION['itemQty'][$i] * $prod[$i]['unit_price'];
+    $subtotal += $total[$i];
+    $grandTotal = $subtotal + 10;
 }
-
-//for ($c = 0; $c < count($_SESSION['itemQty']); $c++){
-//    echo "<p> Item quantity:" . $_SESSION['itemQty'][$c] . "
-//   Product ID: " . $_SESSION['prod_id'][$c] ."</p>";
-//}
-
 
 echo "
     <div class='container'>
@@ -39,11 +28,8 @@ echo "
             </tr>
             </thead>
            ";
-//print count($_SESSION['itemQty']);
-for ($m = 0; $m < count($_SESSION['itemQty']); $m++) {
-//print $prod['prod_name'];
-//print "<br>";
 
+for ($m = 0; $m < count($_SESSION['itemQty']); $m++) {
     echo "
                 <tbody>
                 <tr><td><h5>" . $prod[$m]['prod_name'] . "</h5>
@@ -53,7 +39,7 @@ for ($m = 0; $m < count($_SESSION['itemQty']); $m++) {
                     <td><h5>" . $prod[$m]['unit_price'] . "</h5>
                     </td>
                     <td>
-                        <h5>$total</h5>
+                        <h5>$total[$m]</h5>
                     </td>
                     <td>
                         <button type='button' class='btn btn-danger btn-sm'>
@@ -84,7 +70,7 @@ echo "
                     <td></td>
                     <td></td>
                     <td><h5>Total</h5></td>
-                    <td><h5>$grandtotal</h5></td>
+                    <td><h5>$grandTotal</h5></td>
                 </tr>
                 <tr>
                     <td>
