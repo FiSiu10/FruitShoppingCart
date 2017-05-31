ea3b257238811de8cbd984e4ff50e53545833865
Fiona Siu <fiona.siu10@gmail.com>
2017-05-30 19:57:48 -0700

    disable SMTP debug

--- src/confirmationEmail.php
+++ src/confirmationEmail.php
@@ -16,7 +16,7 @@ function sendConfirmationEmail($email, $name, $shipAddress, $shipCity, $shipProv
     // 0 = off (for production use)
     // 1 = client messages
     // 2 = client and server messages
-    $mail->SMTPDebug = 2;
+    $mail->SMTPDebug = 0;
 
     //Ask for HTML-friendly debug output
     $mail->Debugoutput = 'html';
