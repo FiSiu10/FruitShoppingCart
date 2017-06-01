77ceffe711fe32c9df4c7b7a8310fd03aa55d786
Kerensa Mattison <kcamosun@gmail.com>
2017-06-01 13:02:33 -0700

    Layout changes to FAQ, about us, & shipping time consistency.

--- src/view/faq.php
+++ src/view/faq.php
@@ -4,57 +4,53 @@
 
 ?>
 
-<div class="container-fluid bg-2 text-center">
-<h4>FACTS AND QUESTIONS</h4>
-</div>
-<br>
-    <div class="container">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-8"><h4>Shipping Info</h4></div>
-            </div>
-			 <div class="row">
-                <div class="col-sm-8">
-				<p>
-We ship to Canada and guarantee shipment within a week. The products come in cases and are packed with cooling packs to ensure freshness in maintained. The shipping price is a flat rate of $10 for all products.
-				</p>
-				</div>
-            </div>
-		</div>
-	</div>
-	<br>
-	    <div class="container">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-8"><h4>Returns</h4></div>
-            </div>
-			 <div class="row">
-                <div class="col-sm-8">
-				<p>
-If you are unhappy or have any issues with your shipment please return it for a full refund (minus shipping costs). We take returns within two weeks of the purchase date. Please ship returns back at your expense to The Exotic Fruit Company (see the return address included with your shipment).
-				</p>
-				</div>
-            </div>
-		</div>
-	</div>
-	<br>
-		    <div class="container">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-8"><h4>Freshness Guarantee</h4></div>
-            </div>
-			 <div class="row">
-                <div class="col-sm-8">
-				<p>
-We guarantee that you will receive your items fresh and free from rot. We ship within a week an include cooling packs in order to ensure freshness. If you have any issues please return the product for a refund (within two weeks of purchase date at your expense).
-				</p>
-				</div>
-            </div>
-		</div>
-	</div>
-    <br>
-    <br>
+<style>
+    p {
+       margin: 10px 600px;
+    }
+	
+	/* Small tablets and mobile ----------- */
+@media only screen  and (min-width : 400px) {
+    p {
+       margin: 10px 100px;
+    }
+	
+	
+		/* Laptops and tablets ----------- */
+@media only screen  and (min-width : 800px) {
+    p {
+       margin: 10px 200px;
+    }
+	
+	
+	/* Desktops and laptops ----------- */
+@media only screen  and (min-width : 1224px) {
+    p {
+       margin: 10px 400px;
+    }
+	
+	/* Large screens ----------- */
+@media only screen  and (min-width : 1824px) {
+    p {
+       margin: 10px 600px;
+    }		
+</style>
 
+<div class="container-fluid bg-2 text-center">
+<h1>Facts and Questions</h1>
+<br>
+	<h4>Shipping Info</h4>
+		<p>We ship to Canada and guarantee your order will be shipped in 24 hours. The products come in cases, as individual items, or as noted on the fruit description page. Items are packed with cooling packs to ensure freshness in maintained. The shipping price is a flat rate of $10 for all products.</p>
+<br>
+	<h4>Returns</h4>
+		<p>If you are unhappy or have any issues with your shipment please return it for a full refund (minus shipping costs). We take returns within two weeks of the purchase date. Please ship returns back at your expense to The Exotic Fruit Company (see the return address included with your shipment).</p>
+<br>
+	<h4>Freshness Guarantee</h4>
+		<p>We guarantee that you will receive your items fresh and free from rot. We ship within 24 hours and include cooling packs in order to ensure freshness. If you have any issues please return the product for a refund (within two weeks of purchase date at your expense).</p>
+</div>
+
+    <br>
+    <br>
 
 </body>
 
