271147c1959dbd47ffbb7f99c0c7c22a851a7ee9
Fiona Siu <fiona.siu10@gmail.com>
2017-06-01 18:03:59 -0700

    formatted fruit info page

--- src/allFruitInfo.php
+++ src/allFruitInfo.php
@@ -16,42 +16,54 @@ for ($i = 0; $i < count($productInfo); $i++){
 $productStock = $prod['stock_amount'];
 
 ?>
+<style>
+    .btn{
+        margin-right: 90px;
+    }
+    .product{
+        margin-top: 20px;
+    }
+    .price{
+        color: red;
+    }
 
+</style>
 <div class="container">
   <div class="container-fluid">
     <?php
       echo
        "<div class='row'>
-            <div class='col-sm-4'><img src='" . $prod['photo'] . "' width='350px' height='250px'></div>
-            <div class='col-sm-6'><h2>" . $prod['prod_name'] . "</h2></div>
-            <div class='col-sm-6'><h4>PRICE: $ " . " " . $prod['unit_price'] . "</h4></div>
-            <div class='col-sm-6'><br></div>
-            <div class='col-sm-6'><h5>Description: </div>
-            <div class='col-sm-6'>" . $prod['prod_desc'] ."</div>
+            <div class='col-sm-4 product'><img src='" . $prod['photo'] . "' width='350px' height='250px'></div>
+            <div class='col-sm-6'><h1>" . $prod['prod_name'] . "</h1></div>
+            <div class='col-sm-6'></div>
+            <div class='col-sm-6'><hr></div>
+            <div class='col-sm-6'></div>
+            <div class='col-sm-6'><h4>" . $prod['prod_desc'] ."</h4></div>
         </div>
         "
     ?>
 
     <div class="row">
         <div class="col-sm-4"></div>
-        <div class='col-sm-4'></div>
+        <div class="col-sm-4 price"><h3><?php echo "Price: $" . " " . $prod['unit_price'] . " " ?></h3></div>
         <div class="col-sm-4">
             <div class="dropdown">
               <form action="addToCart.php" method="post" >
-                <h4>Quantity:
+                <h3>Quantity:
                   <input type="number" style="width:150px" min="1" max="<?php print $productStock; ?>"name="quantity" placeholder="Stock: <?php print $productStock; ?>" />
                   <input type="hidden" name="prod_id" value="<?php print $prod_id; ?>"/>
                   <input type="hidden" name="stock" value="<?php print $productStock; ?>">
-                </h4>
+                </h3>
             </div>
         </div>
     </div>
       <div class="row">
             <div class="col-sm-4"></div>
-            <div class='col-sm-4'></div>
+            <div class="col-sm-4"></div>
             <div class="col-sm-4">
-                    <button type="submit" class="btn btn-primary btn-md">Add to Cart</button>
+                    <br><button type="submit" class="btn btn-primary btn-md pull-right">Add to Cart</button>
             </div>
+            <div class="col-sm-4"></div>
             </form>
       </div>
 
